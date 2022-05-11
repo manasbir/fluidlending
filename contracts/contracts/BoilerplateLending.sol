ac14349ffc1b64a1aabe5371a17a75b5557c23de
manas <bagrimanasbir@gmail.com>
2022-05-11 03:03:13 -0700

    Update BoilerplateLending.sol

--- contracts/contracts/BoilerplateLending.sol
+++ contracts/contracts/BoilerplateLending.sol
@@ -4,12 +4,10 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 //standard aave-esque lending
 contract BoilerplateLending {
-    //only DAI for wETH loan for now
-    IERC20 DAI = IERC20(/* insert dai address */);
-    IERC20 wETH = IERC20(/* insert wETH address */);
+
     //some sort of storage for what is valid collateral
 
-    function loan(uint _borrowAmount, uint _collateralAmount) external {
+    function loan(uint _borrowAmount, uint _collateralAmount, address _collateralToken, address  _assetBorrowed) external {
         //require for healthfactor stuff eg require(_collateralAmount > _borrowAmount * 1.5, "not enough collateral");
         wETH.approve(address(this), _collateralAmount);
         wETH.transferFrom(msg.sender, address(this), _collateralAmount);
