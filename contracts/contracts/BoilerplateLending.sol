2944210f4ef5ae4bb30a194e9f5f7ea30624c9d6
manas <bagrimanasbir@gmail.com>
2022-05-11 03:21:35 -0700

    Update BoilerplateLending.sol

--- contracts/contracts/BoilerplateLending.sol
+++ contracts/contracts/BoilerplateLending.sol
@@ -4,19 +4,22 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 //standard aave-esque lending
 contract BoilerplateLending {
-
+    mapping (address => uint) collateralToToken;
     //some sort of storage for what is valid collateral
 
-    function loan(uint _borrowAmount, uint _collateralAmount, address _collateralToken, address  _assetBorrowed) external {
+    function loan(uint _borrowAmount, address _collateralToken, address  _assetBorrowed) external {
         //require for healthfactor stuff eg require(_collateralAmount > _borrowAmount * 1.5, "not enough collateral");
-        wETH.approve(address(this), _collateralAmount);
-        wETH.transferFrom(msg.sender, address(this), _collateralAmount);
+        //require token to be applicable so we dont get useless shitcoins
+        require _collateralToken 
+        uint collateralRatio = collateralToToken[_collateralToken] * 100; // x100 for percentage
+        IERC20(_collateralToken).approve(address(this), collateralRatio);
+        IERC20(_collateralToken).transferFrom(msg.sender, address(this), collateralRatio);
 
-        DAI.transferFrom(address(this), msg.sender, _borrowAmount);
+        IERC20(_assetBorrowed).transferFrom(address(this), msg.sender, _borrowAmount);
 
         //interest adding function or whatever
         //we need some way to liquidate or some way to set a loan to liquidatable
-        //do we need to store all of our current loans in a struct/mapping
+        //do we need to store all of our current loans in a struct/mapping??
         //https://github.com/aave/protocol-v2/blob/master/contracts/protocol/lendingpool/LendingPool.sol
         //use ctrl + F _executeBorrow
     }
