574446b4dea36bee7b2af733b5c3b700e861a9b7
manas <bagrimanasbir@gmail.com>
2022-05-11 00:42:30 -0700

    added some boilerplate code to get started

---
+++ contracts/contracts/LendingProxy.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./UnderCollateralizedLending.sol";
+import "./SuperFluidLending.sol";
+import "./Crosschain.sol";
+import "./StablecoinsLending.sol";
+
+contract proxy {
+    
+}
+
