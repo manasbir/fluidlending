339b956491e234ad1e536191cb97edf5d9a91346
manas <bagrimanasbir@gmail.com>
2022-05-22 01:41:23 -0700

    sdsS

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,8 +1,67 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import * as React from "react";
+import { ethers } from "ethers";
+import abi from "./utils/BoilerplateLending.json"
 
 export default function Home() {
+  const [currAccount, setCurrentAccount] = React.useState("")
+  const contractAddress = "0x57C65EB151c772b5e733D5ebe91120619a06b4e7"
+  const contractABI = abi.abi
+
+  const checkIfWalletIsThere = () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log('no eth XD')
+    } else {
+      console.log('finally, eth', ethereum);
+    }
+
+    ethereum.request({ method: 'eth_accounts' })
+      .then(accounts => {
+        console.log(accounts)
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('we have an authorized accoutn')
+          setCurrentAccount(account)
+          getAllWaves()
+        } else {
+          console.log('user has no perms for us to use')
+        }
+      })
+  }
+
+  const connectWallet = () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert('you probably wanna connect your wallet')
+    }
+
+    ethereum.request({ method: 'eth_requestAccounts' })
+      .then(accounts => {
+        console.log(accounts[0])
+        setCurrentAccount(accounts[0])
+      })
+      .catch(err => console.warn(err))
+  }
+
+  const borrow = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner()
+    const boilerplateLendingContract = new ethers.Contract(contractAddress, contractABI, signer);
+    //ETHx, DAIx, USDCx, TUSDx
+    //0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1, 0xe3CB950Cb164a31C66e32c320A800D477019DCFF, 0x25B5cD2E6ebAedAa5E21d0ECF25A567EE9704aA7, 0xB20200908d60f1d7bc68594f677bC15070a87504
+
+    console.log('what msg?', message)
+    const borrow = await boilerplateLendingContract.borrow(_asset, _amount, _borrowedAsset, _amountBorrowed /* { gasLimit: 300000 } */)
+
+    console.log('mining....', waveTxn.hash)
+    await borrow.wait()
+    console.log('Mined -- ', waveTxn.hash)
+  }
+  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,41 +74,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
