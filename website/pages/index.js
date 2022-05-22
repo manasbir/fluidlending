93bde708a3ab454fb3641b14538c55fa363f9811
Brian Lee <LSH245@hotmail.com>
2022-05-22 21:14:34 +1200

    WIP

--- website/pages/index.js
+++ website/pages/index.js
@@ -3,6 +3,13 @@ import { ethers } from "ethers";
 import abi from "./utils/BoilerplateLending.json"
 import Button from './components/Button';
 
+const Collaterals = Object.freeze({
+  ETHx: "0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1",
+  DAIx: "0xe3CB950Cb164a31C66e32c320A800D477019DCFF",
+  USDCx: "0x25B5cD2E6ebAedAa5E21d0ECF25A567EE9704aA7",
+  TUSDx: "0xB20200908d60f1d7bc68594f677bC15070a87504"
+});
+
 export default function Home() {
   const inputStyle = {
     height: 30,
@@ -10,15 +17,15 @@ export default function Home() {
   };
 
   const [currAccount, setCurrentAccount] = React.useState("");
-  const [collateral, setCollateral] = React.useState("0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1");
-  const [collateralAmount, setCollateralAmount] = React.useState("");
-  const [borrowableTokens, setBorrowableTokens] = React.useState("0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1");
-  const [amountBorrowing, setAmountBorrowing] = React.useState("");
+  const [collateral, setCollateral] = React.useState(Collaterals.ETHx);
+  const [collateralAmount, setCollateralAmount] = React.useState(null);
+  const [borrowableTokens, setBorrowableTokens] = React.useState(Collaterals.ETHx);
+  const [amountBorrowing, setAmountBorrowing] = React.useState(null);
   const [isConnected, setIsConnected] = React.useState(false);
   const contractAddress = "0x57C65EB151c772b5e733D5ebe91120619a06b4e7";
   const contractABI = abi.abi;
 
-  const checkIfWalletIsThere = () => {
+  const checkIfWalletIsThere = async () => {
     const { ethereum } = window;
     if (!ethereum) {
       console.log('no eth XD')
@@ -26,32 +33,31 @@ export default function Home() {
       console.log('finally, eth', ethereum);
     }
 
-    ethereum.request({ method: 'eth_accounts' })
-      .then(accounts => {
-        console.log(accounts)
-        if (accounts.length !== 0) {
-          const account = accounts[0];
-          console.log('we have an authorized accoutn')
-          setCurrentAccount(account)
-        } else {
-          console.log('user has no perms for us to use')
-        }
-      })
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    console.log(accounts)
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('we have an authorized accoutn')
+      setCurrentAccount(account)
+    } else {
+      console.log('user has no perms for us to use')
+    }
   }
 
-  const connectWallet = () => {
+  const connectWallet = async () => {
     const { ethereum } = window;
     if (!ethereum) {
       alert('you probably wanna connect your wallet')
     }
 
-    ethereum.request({ method: 'eth_requestAccounts' })
-      .then(accounts => {
-        console.log(accounts[0])
-        setCurrentAccount(accounts[0])
-        setIsConnected(true);
-      })
-      .catch(err => console.warn(err))
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      console.log(accounts[0])
+      setCurrentAccount(accounts[0])
+      setIsConnected(true);
+    } catch (error) {
+      console.warn(error)
+    }
   }
 
   const borrow = async () => {
@@ -59,11 +65,11 @@ export default function Home() {
     const signer = provider.getSigner()
     const boilerplateLendingContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    const borrow = await boilerplateLendingContract.borrow(collateral, collateralAmount * 1000000000000000000, borrowableTokens, amountBorrowing * 1000000000000000000, { gasLimit: 300000 })
+    const response = await boilerplateLendingContract.borrow(collateral, BigInt(collateralAmount * 1000000000000000000), borrowableTokens, BigInt(amountBorrowing * 1000000000000000000), { gasLimit: 300000 })
 
-    console.log('mining....', borrow.hash)
+    console.log('mining....', response.hash)
     await borrow.wait()
-    console.log('Mined -- ', borrow.hash)
+    console.log('Mined -- ', response.hash)
   }
 
   React.useEffect(() => {
@@ -100,29 +106,29 @@ export default function Home() {
         <div>
           <label>Collateral: </label>
           <select value={collateral} onChange={(x) => setCollateral(x.target.value)}>
-            <option value="0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1">ETHx</option>
-            <option value="0xe3CB950Cb164a31C66e32c320A800D477019DCFF">DAIx</option>
-            <option value="0x25B5cD2E6ebAedAa5E21d0ECF25A567EE9704aA7">USDCx</option>
-            <option value="0xB20200908d60f1d7bc68594f677bC15070a87504">TUSDx</option>
+            <option value={Collaterals.ETHx}>ETHx</option>
+            <option value={Collaterals.DAIx}>DAIx</option>
+            <option value={Collaterals.USDCx}>USDCx</option>
+            <option value={Collaterals.TUSDx}>TUSDx</option>
           </select>
         </div>
 
         <div>
-          <label>Collateral Amount:</label>
+          <label>Collateral Amount: </label>
           <input type="number" style={inputStyle} value={collateralAmount} onChange={(event) => setCollateralAmount(event.target.value)} />
         </div>
 
         <div> <label>Borrowable Tokens: </label>
           <select value={borrowableTokens} onChange={(x) => setBorrowableTokens(x.target.value)}>
-            <option value="0xDD5462a7dB7856C9128Bc77Bd65c2919Ee23C6E1">ETHx</option>
-            <option value="0xe3CB950Cb164a31C66e32c320A800D477019DCFF">DAIx</option>
-            <option value="0x25B5cD2E6ebAedAa5E21d0ECF25A567EE9704aA7">USDCx</option>
-            <option value="0xB20200908d60f1d7bc68594f677bC15070a87504">TUSDx</option>
+            <option value={Collaterals.ETHx}>ETHx</option>
+            <option value={Collaterals.DAIx}>DAIx</option>
+            <option value={Collaterals.USDCx}>USDCx</option>
+            <option value={Collaterals.TUSDx}>TUSDx</option>
           </select>
         </div>
 
         <div>
-          <label>Amount Borrowing:</label>
+          <label>Amount Borrowing: </label>
           <input type="number" style={inputStyle} value={amountBorrowing} onChange={(event) => setAmountBorrowing(event.target.value)} />
         </div>
         {!isConnected && <Button style={{ height: 40 }} onClick={borrow}>Borrow</Button>}
